173b277391af0c1b21e88b1620d6b21c465f1024
Michael <miccarpino@gmail.com>
2023-04-25 21:19:15 +0200

    Search news functionality

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object {
         const val API_KEY = "c93e8b26d2ed455ba40efd9e2d9d946f"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
