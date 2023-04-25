b5999f2a1ccdf773cd53647675181158dd012e8c
Michael <miccarpino@gmail.com>
2023-04-25 19:04:55 +0200

    Retrofit setup, API response models

---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "c93e8b26d2ed455ba40efd9e2d9d946f"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
