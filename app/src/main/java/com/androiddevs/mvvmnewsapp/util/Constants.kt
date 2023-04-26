5d386289d42cce8df349e699fd28b68a1e9c2238
Michael <miccarpino@gmail.com>
2023-04-26 19:44:37 +0200

    Pagination functionality

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -5,5 +5,7 @@ class Constants {
         const val API_KEY = "c93e8b26d2ed455ba40efd9e2d9d946f"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
     }
 }
+
